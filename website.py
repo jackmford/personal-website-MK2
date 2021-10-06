0dcf04959ac6d461707efd84615f4d4b67cfafc1
jackmford <jackmitchellfordyce.com>
2021-10-06 00:19:41 +0000

    moving files

--- website.py
+++ website.py
@@ -1,9 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "HI"
+from flask import render_template
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/resume/')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
