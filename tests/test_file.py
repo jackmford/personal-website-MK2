17739b13ef5c4d6706f3f686e64a0134f733ff99
jackmford <jackmitchellfordyce@gmail.com>
2022-05-19 14:34:17 -0500

    feat: adding tests for resume and health endpoints

--- tests/test_file.py
+++ tests/test_file.py
@@ -7,3 +7,17 @@ def test_homepage_endpoint():
     with app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
+
+def test_resume():
+    app = create_app()
+
+    with app.test_client() as test_client:
+        response = test_client.get('/resume')
+        assert response.status_code == 308
+
+def test_health():
+    app = create_app()
+
+    with app.test_client() as test_client:
+        response = test_client.get('/health')
+        assert response.status_code == 200
